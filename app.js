dd144031c63cf254b12dfbe1592862f7a2b0162d
Pdutra <powdutra26@gmail.com>
2022-11-21 19:33:20 -0300

    Versão com todas funcionalidades e testada

--- app.js
+++ app.js
@@ -16,12 +16,22 @@ const categorias = [{
     name: 'Comportamental/Soft'
 }]
 
+const filtroCategorias = [true, ...categorias.map(() => false)] //Corresponde ao status de exibição de cada categoria (true = ativo/visible e false = inativo/hidden) 
+
 //Adiciona as categorias definidas no começo do código ao "select" do formulário
 
 function addCategorias() {
     const select = document.getElementById('categoria')
     const menuCards = document.getElementById('menuDeCards')
 
+    //Cria botão de total de dicas
+    const botaoTotal = document.createElement('button')
+    botaoTotal.innerHTML = 'Total<br><span class="numeroCard">0</span>'
+    botaoTotal.id = "botao_0"
+    menuCards.appendChild(botaoTotal)
+    botaoTotal.addEventListener('click', filtraCategorias)
+
+    //Cria elementos baseado na quantidade de categorias
     categorias.forEach((categoria) => {
         // Cria opções para o select
         const option = document.createElement('option')
@@ -34,6 +44,7 @@ function addCategorias() {
         botao.innerHTML = `${categoria.name}<br><span class="numeroCard">0</span>`
         botao.id = 'botao_' + categoria.value
         menuCards.append(botao)
+        botao.addEventListener('click', filtraCategorias)
     })
     atualizaCards()
 }
@@ -60,12 +71,7 @@ function addDica(evento) {
         id,
     evento.target.linkYT.value
     ]
-    // let urlValida = ''
-    // if (evento.target.linkYT.value !== '') {
-    //     urlValida = validaURL(evento.target.linkYT.value)
-    // }
     const dica = new Dica(...atributos)
-
     dicas.push(dica)
     htmlDica(dica, true)
     atualizaCards()
@@ -87,15 +93,15 @@ function htmlDica(objDica, novaDica) {
     }
     let botaoYT = ''
     if (objDica.linkYT !== '') {
-        botaoYT = `<a href=${objDica.linkYT} target="_blank"><button>Youtube</button></a>`
+        botaoYT = `<a href=${objDica.linkYT} target="_blank"><button><img src="images/next.png" alt="Youtube link">Youtube</button></a>`
     }
     dica.innerHTML = `<h3>${objDica.titulo}</h3>
     <p><strong>Linguagem/Skill:</strong>  ${objDica.linguagemSkill}</p>
     <p><strong>Categoria:</strong>  ${objDica.categoria}</p>
     <p>${objDica.descricao}</p>
     <div class='botoesDica'>
-    <button type="button" id='excluir_${objDica.id}'>Excluir</button>
-    <button type="button" id='editar_${objDica.id}'>Editar</button>
+    <button type="button" id='excluir_${objDica.id}'><img src="images/trash.png" alt="Excluir">Excluir</button>
+    <button type="button" id='editar_${objDica.id}'><img src="images/document.png" alt="Editar">Editar</button>
     ${botaoYT}
     </div>`
 
@@ -131,7 +137,6 @@ function excluirDica(evento) {
 
 function formEdicaoDica(evento) {
     const formulario = document.getElementById('formulario')
-
     const idDica = (evento.target.id).split("_")[1]
     const posDica = dicas.map((elemento) => elemento.id).indexOf(idDica)
     formulario.className = "dica_" + idDica
@@ -195,21 +200,22 @@ function salvaAlteracoes() {
 
 function renderizaDicas() {
     dicas.forEach((dica) => htmlDica(dica, true))
+    atualizaCards()
 }
 
 function ativaTodasDicas() {
     dicas.forEach(dica => {
         const elementoDica = document.getElementById(dica.id)
-        elementoDica.className = ''
-        const dicaChildren = elementoDica.children 
+        elementoDica.classList.remove('inativo')
+        const dicaChildren = elementoDica.children
 
         const htmlTitulo = dicaChildren[0].innerHTML.split('</span>')
-        if (htmlTitulo.length !== 1){ //Quer dizer que há um elemento <span>
+        if (htmlTitulo.length !== 1) { //Quer dizer que há um elemento <span>
             dicaChildren[0].innerHTML = htmlTitulo[0].replace('<span class="termoPesquisado">', '') + htmlTitulo[1]
         }
 
         const htmlDescricao = dicaChildren[3].innerHTML.split('</span>')
-        if (htmlDescricao.length !== 1){ //Quer dizer que há um elemento <span>
+        if (htmlDescricao.length !== 1) { //Quer dizer que há um elemento <span>
             dicaChildren[3].innerHTML = htmlDescricao[0].replace('<span class="termoPesquisado">', '') + htmlDescricao[1]
         }
     });
@@ -218,9 +224,9 @@ function ativaTodasDicas() {
 function fazPesquisa(termoPesquisado) {
     dicas.forEach(dica => {
         const elementoDica = document.getElementById(dica.id)
-        if (dica.titulo.includes(termoPesquisado) || dica.descricao.includes(termoPesquisado)) { 
+        if (dica.titulo.includes(termoPesquisado) || dica.descricao.includes(termoPesquisado)) {
             //Retorna lista com os elementos filhos da Dica (li)
-            const dicaChildren = elementoDica.children 
+            const dicaChildren = elementoDica.children
             // [h3(título), p, p, p(descrição), div]
 
             //Cria elemento span em volta do termo pesquisado (título)
@@ -229,9 +235,63 @@ function fazPesquisa(termoPesquisado) {
             //Cria elemento span em volta do termo pesquisado (descrição)
             dicaChildren[3].innerHTML = dicaChildren[3].innerHTML.replace(termoPesquisado, '<span class="termoPesquisado">' + termoPesquisado + '</span>')
         } else {
-            elementoDica.className = 'inativo' //Desativa dicas que não contem o termo pesquisado
+            elementoDica.classList.add('inativo') //Desativa dicas que não contem o termo pesquisado
         }
     });
+}
+
+function filtraCategorias(evento) {
+    let idBotao
+    if (evento.target.id === '') {
+        idBotao = evento.target.parentNode.id //Caso o usuário clique no elemento <span> (id === '')
+    } else {
+        idBotao = evento.target.id
+    }
+    const categoria = Number(idBotao.split('_')[1])
+    if (categoria != 0){
+        filtroCategorias[0] = false
+    }
+    filtroCategorias[categoria] = !filtroCategorias[categoria]
+
+    const botoesAtivados = filtroCategorias.reduce((acc, elemento) => {
+        if(elemento){
+            acc++
+        }
+        return acc
+    },0)
+
+    if (botoesAtivados === filtroCategorias.length - 1 || botoesAtivados===0) {
+        filtroCategorias[0] = true
+    }
+
+    if (filtroCategorias[0]) {
+        document.getElementById('botao_0').className = 'selecionado'
+        for (let i = 1; i < filtroCategorias.length; i++) {
+            document.getElementById('botao_' + i).className = ''
+            filtroCategorias[i] = false
+        }
+        dicas.forEach(dica => {
+            document.getElementById(dica.id).classList.remove('invisivel')
+        })
+    }else{
+        document.getElementById('botao_0').className = ''
+        for (let i = 1; i < filtroCategorias.length; i++) {
+            if(filtroCategorias[i]){
+                document.getElementById('botao_'+ i).className = 'selecionado'
+            }else{
+                document.getElementById('botao_' + i).className = ''
+            }
+        }
+        dicas.forEach(dica => {
+            if (filtroCategorias[Number(dica.value)]) {
+                document.getElementById(dica.id).classList.remove('invisivel')
+            } else {
+                document.getElementById(dica.id).classList.add('invisivel')
+            }
+        })
+    }
+
+    
 }
 
 window.addEventListener('load', addCategorias)
